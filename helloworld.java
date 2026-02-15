08214b1c5e8a429e7a0b5f0568fbd26f2118a3bc
Shafeeqgs <gshafeeq93@gmail.com>
2026-02-15 19:58:44 +0530

    modified commit

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,6 @@
 public class helloworld {
     public static void main(String[] args) {
         System.out.println("welcome to DevOps");
+        System.out.println("welcome to DevOps Day2");
     }
 }
